8fab0302ec0f3911b4d1cbd549a9c02b8dca0d27
Jack Norquist <jack.norquist@outlook.com>
2024-04-24 19:43:12 -0500

    THis is hard

--- bsts/insert-recur/insertRecur.ts
+++ bsts/insert-recur/insertRecur.ts
@@ -1,9 +1,45 @@
-import { BSTNum } from "../common/bst";
+import { BSTNum, BNodeNum } from "../common/bst";
 
 /** insertRecursively(val): Insert a new node into the BST with value val.
  * Uses recursion. */
 
 function insertRecur(bst: BSTNum, val: number): void {
+
+
+
+  if (!bst.root) {
+    bst.root = (new BNodeNum(val));
+    return;
+  }
+
+
+
+  function checkNode(node: BNodeNum | null, val: number) {
+
+    //if right is null and value is bigger than the current node -> insert
+    if (!node!.left && val < node!.val) {
+      node!.left === new BNodeNum(val);
+      return;
+    }
+    //if left is null and value is bigger than the current node -> insert
+    if (!node!.right && val > node!.val) {
+      node!.right === new BNodeNum(val);
+      return;
+    }
+
+    const checkNode1 = val > node!.val ? node!.right : node!.left;
+    console.log(checkNode1);
+
+    checkNode(checkNode1, val);
+
+
+  }
+
+
+  checkNode(bst.root, val);
+
+
+
 }
 
 export { insertRecur };
